5d8b2240651cda32f14d5c1d698ecfe74d46e3b3
Oluwatoni Adeniyi <ol618725@dal.ca>
2024-10-11 00:07:29 -0300

    Replace script.js

--- labs/lab4/js/script.js
+++ labs/lab4/js/script.js
@@ -5,39 +5,63 @@ let emailRegex = /([A-z0-9])+[-.A-z0-9]+[@][a-z]+.[a-z]+.[a-z]{1,6}/;
 let firstName = document.getElementById("firstName");
 let lastName = document.getElementById("lastName");
 let email = document.getElementById("email");
-let countries = document.getElementsByTagName("option")[0];
+let countries = document.getElementsByTagName("select")[0];
 let male = document.getElementById("male");
 let female = document.getElementById("female");
 let none = document.getElementById("none");
 let nonbinary = document.getElementById("nonbinary");
 let body = document.getElementsByTagName("body")[0];
+let result = document.createElement("div");
 
 let submitBtn = document.getElementById("submitBtn");
-submitBtn.addEventListener("submit", function (evt) {
+submitBtn.addEventListener("click", function (evt) {
   evt.preventDefault();
   validate();
 });
 
 function validate() {
-  // result.appendChild(displayFirstName());
-  // result.appendChild(displayLastName());
-  // result.appendChild(displayEmail());
+  let firstNameValid = validateFirstName(firstName);
+  let lastNameValid = validateLastName(lastName);
+  let emailValid = validateEmail(email);
+  let genderValid = validateGender();
+  let countryValid = validateCountry(countries);
+
   if (
-    validateFirstName(firstName) &&
-    validateLastName(lastName) &&
-    validateEmail(email)
+    firstNameValid &&
+    lastNameValid &&
+    emailValid &&
+    genderValid &&
+    countryValid
   ) {
     displayResult();
   } else {
-    displayErrors();
+    displayErrors(
+      firstNameValid,
+      lastNameValid,
+      emailValid,
+      genderValid,
+      countryValid
+    );
   }
 }
 
-function displayErrors() {
+function displayErrors(first, last, mail, gender, country) {
   let result = document.createElement("div");
-  result.appendChild(displayFirstNameError());
-  result.appendChild(displayFirstNameError());
-  result.appendChild(displayFirstNameError());
+  if (!first) {
+    result.appendChild(displayFirstNameError());
+  }
+  if (!last) {
+    result.appendChild(displayLastNameError());
+  }
+  if (!mail) {
+    result.appendChild(displayEmailError());
+  }
+  if (!gender) {
+    result.appendChild(displayGenderError());
+  }
+  if (!country) {
+    result.appendChild(displayCountryError());
+  }
   body.appendChild(result);
 }
 
@@ -61,30 +85,17 @@ function validateFirstName(args) {
 }
 
 function displayFirstName() {
-  if (validateFirstName(firstName)) {
-    let firstNameResult = document.createElement("p");
-    firstNameResult.textContent = args.value;
-    return firstNameResult;
-  }
-  // else {
-  // let firstNameError = document.createElement("p");
-  // firstNameError.style.color = "red";
-  // firstNameError.textContent =
-  //   "First name is invalid. \n Input must not be empty\n Input must only contain letters";
-  // body.append(firstNameError);
-  // return firstNameError;
-  // }
+  let firstNameResult = document.createElement("p");
+  firstNameResult.textContent = firstName.value;
+  return firstNameResult;
 }
 
 function displayFirstNameError() {
-  if (!validateFirstName(firstName)) {
-    let firstNameError = document.createElement("p");
-    firstNameError.style.color = "red";
-    firstNameError.textContent =
-      "First name is invalid. \n Input must not be empty\n Input must only contain letters";
-    // body.append(firstNameError);
-    return firstNameError;
-  }
+  let firstNameError = document.createElement("p");
+  firstNameError.style.color = "red";
+  firstNameError.textContent =
+    "First name is invalid. \n Input must not be empty\n Input must only contain letters";
+  return firstNameError;
 }
 
 function validateLastName(args) {
@@ -97,21 +108,17 @@ function validateLastName(args) {
 }
 
 function displayLastName() {
-  if (validateLastName(firstName)) {
-    let lastNameResult = document.createElement("p");
-    lastNameResult.textContent = args.value;
-    return lastNameResult;
-  }
+  let lastNameResult = document.createElement("p");
+  lastNameResult.textContent = lastName.value;
+  return lastNameResult;
 }
 
 function displayLastNameError() {
-  if (!validateFirstName(firstName)) {
-    let lastNameError = document.createElement("p");
-    lastNameError.style.color = "red";
-    lastNameError.textContent =
-      "Last name is invalid. \n Input must not be empty\n Input must only contain letters";
-    return lastNameError;
-  }
+  let lastNameError = document.createElement("p");
+  lastNameError.style.color = "red";
+  lastNameError.textContent =
+    "Last name is invalid. \n Input must not be empty\n Input must only contain letters";
+  return lastNameError;
 }
 
 function validateEmail(args) {
@@ -124,30 +131,58 @@ function validateEmail(args) {
 }
 
 function displayEmail() {
-  if (validateEmail(email)) {
-    let emailResult = document.createElement("p");
-    emailResult.textContent = args.value;
-    return emailResult;
-  }
+  let emailResult = document.createElement("p");
+  emailResult.textContent = email.value;
+  return emailResult;
 }
 
 function displayEmailError() {
-  if (!validateFirstName(firstName)) {
-    let emailError = document.createElement("p");
-    emailError.textContent = "Invalid email";
-    emailError.style.color = "red";
-    return emailError;
-  }
+  let emailError = document.createElement("p");
+  emailError.textContent = "Invalid email";
+  emailError.style.color = "red";
+  return emailError;
 }
 
 function displayGender() {
-  let emailResult = document.createElement("p");
-  emailResult.textContent = args.value;
-  return emailResult;
+  let genderResult = document.createElement("p");
+  if (male.checked) {
+    genderResult.textContent = "Male";
+  } else if (female.checked) {
+    genderResult.textContent = "Female";
+  } else if (nonbinary.checked) {
+    genderResult.textContent = "Non-binary";
+  } else if (none.checked) {
+    genderResult.textContent = "Prefer not to answer";
+  }
+  return genderResult;
+}
+
+function validateGender() {
+  return male.checked || female.checked || nonbinary.checked || none.checked;
+}
+
+function displayGenderError() {
+  let genderError = document.createElement("p");
+  genderError.textContent = "Gender not selected";
+  genderError.style.color = "red";
+  return genderError;
+}
+
+function validateCountry() {
+  if (countries.selectedIndex > 0) {
+    return true;
+  } else return false;
 }
 
 function displayCountry() {
   let countryResult = document.createElement("p");
-  countryResult.textContent = args.value;
+  countryResult.textContent = countries[countries.selectedIndex].label;
   return countryResult;
+}
+
+function displayCountryError() {
+  let countryError = document.createElement("p");
+  countryError.textContent = "Country not selected";
+  countryError.style.color = "red";
+  return countryError;
 }
