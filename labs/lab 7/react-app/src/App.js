0fb0c014e86962f0a69909a004dcc4ace5968bf8
oadeniyi <ol618725@dal.ca>
2024-11-05 00:03:59 -0400

    Initial commit

---
+++ labs/lab 7/react-app/src/App.js
@@ -0,0 +1,24 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Welcome!</h1>
+        <p>This is my react website</p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
